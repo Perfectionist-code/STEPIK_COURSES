9fd0175e4b9ab94c4d44a6aed0730940b171dcf7
Сергей Анатольевич <skirdov-ser@yandex.ru>
2025-04-29 16:12:42 +0300

    Задача на поиск партнёров для танцев

--- Добрый_добрый Python ООП/1.py
+++ Добрый_добрый Python ООП/1.py
@@ -1,4 +1,17 @@
-try:
-    print(list(range(10000000000)))
-except MemoryError:
-    print('У пона память сломалась')
+n = int(input())
+lst_m = sorted([int(x) for x in input().split()])
+m = int(input())
+lst_w = sorted([int(x) for x in input().split()])
+cnt = 0
+for i, val_m in enumerate(lst_m):
+    for j, val_w in enumerate(lst_w):
+        try:
+            a = abs(val_m-val_w)
+            if a < 2:
+                cnt += 1
+                lst_m[i] = None
+                lst_w[j] = None
+                break
+        except TypeError:
+            continue
+print(cnt)
