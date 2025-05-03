731893b4fa58449fda3224190f3548e6282ff956
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-05-03 11:03:28 +0300

    Задача 3_11_1 Пицца

--- OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
+++ OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
@@ -1,29 +1,41 @@
-class BankAccount:
-    bank_name = 'Tinkoff Bank'
-    address = 'Москва, ул. 2-я Хуторская, д. 38А'
+class Pizza:
+    ingredients_dict = {'margherita': ['mozzarella', 'tomatoes'],
+                   'peperoni': ['mozzarella', 'peperoni', 'tomatoes'],
+                   'barbecue': ['mozzarella', 'red onion', 'sauce bbq', 'chicken']
+                  }
 
-    def __init__(self, name: str, balance: int):
-        self.name = name
-        self.balance = balance
-
-    @classmethod
-    def create_account(cls, name, deposit):
-        return cls(name, deposit)
+    def __init__(self, ingredients=[]):
+        self.ingredients = list(ingredients)
 
     @classmethod
-    def bank_info(cls):
-        return f'{cls.bank_name} is located in {cls.address}'
+    def barbecue(cls):
+        return cls(cls.ingredients_dict['barbecue'])
 
+    @classmethod
+    def peperoni(cls):
+        return cls(cls.ingredients_dict['peperoni'])
 
-# Ниже код для проверки методов класса BankAccount
-oleg = BankAccount.create_account("Олег Тинкофф", 1000)
-assert isinstance(oleg, BankAccount)
-assert oleg.name == 'Олег Тинкофф'
-assert oleg.balance == 1000
-assert BankAccount.bank_info() == 'Tinkoff Bank is located in Москва, ул. 2-я Хуторская, д. 38А'
+    @classmethod
+    def margherita(cls):
+        return cls(cls.ingredients_dict['margherita'])
 
-ivan = BankAccount.create_account("Ivan Reon", 50)
-assert isinstance(ivan, BankAccount)
-assert ivan.name == 'Ivan Reon'
-assert ivan.balance == 50
-print('Good')
+# bbq = Pizza.barbecue()
+# peperoni = Pizza.peperoni()
+# margherita = Pizza.margherita()
+# print(sorted(bbq.ingredients))
+# print(sorted(peperoni.ingredients))
+# print(sorted(margherita.ingredients))
+
+# margherita_1 = Pizza.margherita()
+# margherita_2 = Pizza.margherita()
+# print(sorted(margherita_1.ingredients))
+# margherita_2.ingredients.append('ham')
+# print(sorted(margherita_2.ingredients))
+# print(sorted(margherita_1.ingredients))
+
+# pizza = Pizza()
+#
+# bbq = pizza.barbecue()
+# peperoni = pizza.peperoni()
+# print(sorted(bbq.ingredients))
+# print(sorted(peperoni.ingredients))
