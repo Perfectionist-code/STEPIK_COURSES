deb7523f5112643e6e2139245a8f28d7c47b22df
Sergey Skirdov <skirdov-ser@yandex.ru>
2025-06-10 22:39:13 +0300

    3.9 Classmethod и staticmethod task 5 Оставил на потом

--- OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
+++ OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
@@ -1,29 +1,55 @@
-class Square:
-    power = 2
+class PrettyPrint:
 
-    def get_value(self, a):
-        return pow(a, self.power)
-
-
-class Cube(Square):
-    power = 3
+    def __str__(self):
+        res = ', '.join([str(key) + '=' + str(value) for key, value in self.__dict__.items()])
+        return f'{self.__class__.__name__}({res})'
 
 
-class Power4(Square):
-    power = 4
+# class Person(PrettyPrint):
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
 
 
-# Напишите определение классов Cube и Power4
+# artem = Person('Artem', 'Egorov', 33)
+# ivan = Person('Ivan', 'Ivanov', 45)
+# print(artem)
+# print(ivan)
 
-assert issubclass(Cube, Square)
-assert issubclass(Power4, Square)
+# class Student(PrettyPrint):
+#     def __init__(self, name, surname, student_id, faculty, specialty):
+#         self.student_id = student_id
+#         self.name = name
+#         self.surname = surname
+#         self.faculty = faculty
+#         self.specialty = specialty
 
-cube = Cube()
-assert cube.get_value(2) == 8
-assert cube.get_value(-17) == -4913
+# student_1 = Student("Иван", "Иванов", 12345, "Физический", "Математика")
+# student_2 = Student("Анна", "Смирнова", 67890, "Химический", "Биология")
+# print(student_1)
+# print(student_2)
+#
+# class Student(PrettyPrint):
+#     def __init__(self, name, surname, student_id, faculty, specialty):
+#         self.student_id = student_id
+#         self.name = name
+#         self.surname = surname
+#         self.faculty = faculty
+#         self.specialty = specialty
+#
+#     def __str__(self):
+#         return f"{self.name} {self.surname}"
+#
+#
+# student_1 = Student("Иван", "Иванов", 12345, "Физический", "Математика")
+# student_2 = Student("Анна", "Смирнова", 67890, "Химический", "Биология")
+# print(student_1)
+# print(student_2)
 
-power4 = Power4()
-assert power4.get_value(5) == 625
-assert power4.get_value(25) == 390625
-
-print('Good')
+# class Person(PrettyPrint):
+#     pass
+#
+#
+# p = Person()
+# print(p)
