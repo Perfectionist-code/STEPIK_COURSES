03ebe68277186fcb18ef77250f75cfdaa49d0ec3
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-06-19 23:55:15 +0300

    1_6_2_ООП_Балакирев

--- Добрый_добрый Python ООП/1.6 The magic method __new__. An example of a Singleton pattern/1_6_2.py
+++ Добрый_добрый Python ООП/1.6 The magic method __new__. An example of a Singleton pattern/1_6_2.py
@@ -1,3 +1,22 @@
+# 1.6 Магический метод __new__. Пример паттерна Singleton
+# 9 из 12 шагов пройдено
+# 10 из 17 баллов  получено
+# Видео-разбор подвига (решение смотреть только после своей попытки):
+#
+# YouTube: https://youtu.be/uE1uf7Qtbh4
+# Смотреть на RuTube
+# Подвиг 7. Объявите класс SingletonFive, с помощью которого можно было бы создавать объекты командой:
+#
+# a = SingletonFive(<наименование>)
+# Здесь <наименование> - это данные, которые сохраняются в локальном свойстве name созданного объекта.
+#
+# Этот класс должен формировать только первые пять объектов. Остальные (шестой, седьмой и т.д.) должны быть ссылкой на последний (пятый) созданный объект.
+#
+# Создайте первые десять объектов класса SingletonFive с помощью следующего фрагмента программы:
+#
+# objs = [SingletonFive(str(n)) for n in range(10)]
+# P.S. В программе на экран ничего выводить не нужно.
+
 class SingletonFive:
     __cnt = 0
     __instance = None
@@ -8,12 +27,13 @@ class SingletonFive:
             cls.__cnt += 1
             cls.__instance = super().__new__(cls)
             print(cls.__instance)
-        return  cls.__instance
+        return cls.__instance
 
     def __init__(self, name):
         self.name = name
 
-objs = [SingletonFive(str(n)) for n in range(10)] # эту строчку не менять
+
+objs = [SingletonFive(str(n)) for n in range(10)]  # эту строчку не менять
 
 for i, obj in enumerate(objs, 1):
     print(f'{i}: Name is "{obj.name}" - {obj}')
