8f0c62535edcd7d9756108f1d9d60da7f809b52b
Сергей Анатольевич <skirdov-ser@yandex.ru>
2025-06-21 22:14:50 +0300

    Практикум по математике и Python 2.2 Математические операции 2_2_1

--- OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
+++ OOP_Object-oriented programming in Python by Egorov Artem/task 1.py
@@ -1,25 +1,39 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
+from math import sqrt
 
 
-class Animal(Protocol):
-    def walk(self) -> None:
+class Shape(ABC):
+
+    @abstractmethod
+    def perimetr(self):
         ...
 
-    def speak(self) -> None:
+    @abstractmethod
+    def area(self):
         ...
 
 
-class Dog:
-    def walk(self) -> None:
-        print("This is a dog walking")
-
-    def speak(self) -> None:
-        print("Woof!")
+class TriangleError(Exception):
+    def __str__(self):
+        return 'Ошибка существования треугольника. Проверьте введённые стороны треугольника.'
 
 
-def make_animal_speak(animal: Animal) -> None:
-    animal.speak()
+class Triangle(Shape):
+    @staticmethod
+    def check_triangle(a, b, c):
+        if not all([a + b > c, a + c > b, b + c > a]):
+            raise TriangleError
+        return True
 
+    def perimetr(self, a, b, c):
+        Triangle.check_triangle(a, b, c)
+        return a + b + c
 
-dog = Dog()
-make_animal_speak(dog)
+    def area(self, a, b, c):
+        p = self.perimetr(a, b, c) / 2
+        return sqrt(p * (p - a) * (p - b) * (p - c))
+
+t = Triangle()
+triangle = (int(input()), int(input()), int(input()))
+print(t.perimetr(*triangle))
+print(t.area(*triangle))
