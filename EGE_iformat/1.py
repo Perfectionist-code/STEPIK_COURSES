eba7a189806968679763a87dcd2d111361c8be2b
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-06-26 21:00:47 +0300

    7_3_5

--- EGE_iformat/1.py
+++ EGE_iformat/1.py
@@ -1,20 +1,20 @@
 # from itertools import permutations
-# table = '24 146 45 12356 34 24'.split()
-# graph = 'DC DE DB CB EB BA BG AG'.split()
-# print('1 2 3 4 5 6')
-# for p in permutations('ABCDEG'):
+# table = '467 35 257 47 236 15 134'.split()
+# graph = 'BA AG AD GE DC CE CF EF'.split()
+# print('1 2 3 4 5 6 7')
+# for p in permutations('ABCDEFG'):
 #     if all(str(p.index(c2)+1) in table[p.index(c1)] for c1, c2 in graph):
 #         print(*p)
-#         break
+#         # break
 
 from itertools import permutations
-
-table = '24 146 45 12356 34 24'.split()
-graph = 'DC DE DB CB EB BA BG AG'.split()
-print('1 2 3 4 5 6')
-for p in permutations('ABCDEG'):
+table = '467 35 257 47 236 15 134'.split()
+graph = 'BA AG AD GE DC CE CF EF'.split()
+print('1 2 3 4 5 6 7')
+for p in permutations('ABCDEFG'):
     print(p)
     for c1, c2 in graph:
+        print(table)
         print(c1, c2)
         print(str(p.index(c2) + 1), table[p.index(c1)])
         a = str(p.index(c2) + 1) in table[p.index(c1)]
@@ -23,6 +23,6 @@ for p in permutations('ABCDEG'):
 
     print('#####' * 10)
 
-    # if all(str(p.index(c2)+1) in table[p.index(c1)] for c1, c2 in graph):
-    #     print(*p)
-    #     break
+#     if all(str(p.index(c2)+1) in table[p.index(c1)] for c1, c2 in graph):
+#         print(*p)
+#         break
