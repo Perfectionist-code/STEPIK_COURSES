348024e807d29eaf3dcc8e716b1739870ee4f661
sk-ser <skirdov-ser@yandex.ru>
2025-08-23 18:34:07 +0300

    Решение задач для подготовки к ЕГЭ по информатике

--- Year course EGE 2025/2.15 (П) Домашнее задание 24.09 (№8)/2_15_1.py
+++ Year course EGE 2025/2.15 (П) Домашнее задание 24.09 (№8)/2_15_1.py
@@ -3,7 +3,8 @@ from itertools import product
 letters = ''.join(sorted('ДИСПАНЕРЗЦЯ'))
 for i, per in enumerate(product(letters, repeat=15), 1):
     word = ''.join(per)
-    print(f'{i}. {word}')
+    if not i % 1_000_000_000:
+        print(f'{i}. {word}')
     if word == 'ДИСПАНСЕРИЗАЦИЯ':
         print(f'{i}. {word}')
         print('Ответ: ', i)
