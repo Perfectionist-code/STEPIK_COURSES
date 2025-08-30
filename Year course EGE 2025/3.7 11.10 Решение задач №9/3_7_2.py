65a1cc86724786ec97a55ac180dcda9da37b9fa8
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-08-30 11:00:29 +0300

    Решение задач для подготовки к ЕГЭ по информатике

--- Year course EGE 2025/3.7 11.10 Решение задач №9/3_7_2.py
+++ Year course EGE 2025/3.7 11.10 Решение задач №9/3_7_2.py
@@ -2,6 +2,6 @@ with open('9-107.txt', 'r') as file:
     cnt = 0
     for str_line in file:
         a, b, c = map(int, str_line.split())
-        if a + b + c == 180:
+        if a == b == c == 60:
             cnt += 1
 print('Ответ:', cnt)
