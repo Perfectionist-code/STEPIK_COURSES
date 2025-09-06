672f01d3a2f734e40c8b77b272de2bc826347133
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-09-06 13:32:42 +0300

    7_15_1 VS_CODE

--- kompege/7. Исследование логических выражений/7_15_1.py
+++ kompege/7. Исследование логических выражений/7_15_1.py
@@ -1,6 +1,6 @@
-for A in range(1, 1000):
+for A in range(1, 3000):
     cnt = 0
-    for x in (d:=range(1, 1000)):
+    for x in (d:=range(1, 3000)):
         if ((not x % A) and (not x % 24) and x % 16) <= (x % A):
             cnt += 1
         else: break
