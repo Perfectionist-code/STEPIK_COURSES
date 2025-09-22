0f44acd693c5ce37ac9581d47d2c87725a21f5ca
Сергей Анатольевич <skirdov-ser@yandex.ru>
2025-09-23 00:37:30 +0300

    Решение различных задач для подготовки к ЕГЭ по информатике

---
+++ kompege/23. Поиск делителей ЗАДАНИЕ 25/23_25_19.py
@@ -0,0 +1,24 @@
+from math import ceil, prod
+
+
+def find_dividers(num: int) -> tuple:
+    cnt = 0
+    res = []
+    for i in range(2, num // 2 + 1):
+        if not num % i:
+            cnt += 1
+            if cnt > 2:
+                return tuple()
+            res.append(i)
+    if cnt <= 1:
+        return tuple()
+    return tuple(res)
+
+
+lst = []
+for n in range(174457, 174505):
+    if (r := find_dividers(n)):
+        lst.append(r)
+    lst.sort(key=prod)
+for tup in lst:
+    print(*tup, prod(tup))
---
+++ kompege/23. Поиск делителей ЗАДАНИЕ 25/23_25_20.py
@@ -0,0 +1,8 @@
+from sympy import primerange, prime
+from math import prod
+
+lst1 = list(primerange(1,7906))
+print(lst1.__len__())
+print(*lst1)
+print(prod(lst1), lst1[-1])
+print(2 **)
