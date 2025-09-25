a3a7fc6c92ab5850d48a02a249256a048fdfcd33
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-09-26 01:15:21 +0300

    Решение задач для подготовки к ЕГЭ по информатике

--- kompege/4. Комбинаторика ЗАДАНИЕ 8/4_8_7.py
+++ kompege/4. Комбинаторика ЗАДАНИЕ 8/4_8_7.py
@@ -2,8 +2,10 @@ from itertools import permutations
 
 
 def check_letters(word, pack):
+    print('----' * 5)
     for per_g in permutations(pack, 2):
         word1 = ''.join(per_g)
+        print(word1)
         if word1 in word:
             return False
     return True
@@ -16,6 +18,6 @@ for per in permutations('АИОУБКЛН'):
     word = ''.join(per)
     if check_letters(word, g_l) and check_letters(word, s_l):
         cnt += 1
-        print(word)
+        # print(word)
 print('-----' * 5)
 print(cnt)
