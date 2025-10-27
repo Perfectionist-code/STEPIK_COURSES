5adbd0cf8b0b312a81afb6680c94e82e61ec991f
Skirdov_Sergey <skirdov-ser@yandex.ru>
2025-10-27 13:32:56 +0300

    Решение задач для подготовки к ЕГЭ по информатике

---
+++ Year course EGE 2026/2.22 03.10. Задачи №15 на отрезки и множества/2_22_6.py
@@ -0,0 +1,27 @@
+# def f(x):
+#     P = 25 <= x <= 42
+#     Q = 1 <= x <= 98
+#     A = a1 <= x <= a2
+#     return Q <= ((not P and Q) <= A)
+#
+#
+# ox = [dx for x in (1, 25, 42, 98) for dx in (x, x + 0.1, x - 0.1)]
+#
+# for a1 in ox:
+#     for a2 in ox:
+#         if a2 > a1 and all(f(x) for x in ox):
+#             print(a1,a2)
+
+def f(x):
+    P = 25 <= x <= 42
+    Q = 1 <= x <= 98
+    A = a1 <= x <= a2
+    return (((not P) and Q) <= A)
+
+
+ox = [dx for x in (1, 25, 42, 98) for dx in (x, x + 0.1, x - 0.1)]
+
+for a1 in ox:
+    for a2 in ox:
+        if a2 > a1 and all(f(x) for x in ox):
+            print(a1, a2)
